84d59cb309a4503c39b26e7c013765bd64329d06
+ <+>
2024-07-12 23:26:59 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    // Get the task description input
+    const taskInput = document.getElementById("new-task-description");
+    const taskDescription = taskInput.value;
+    
+    // Create new task list item
+    const taskItem = document.createElement("li");
+    taskItem.innerText = taskDescription;
+    // Append new task to task list
+    taskList.appendChild(taskItem);
+    
+    // Clear input field for next task
+    taskInput.value = "";
+  });
 });
+
